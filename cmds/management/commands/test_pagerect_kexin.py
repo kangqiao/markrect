a1d1b112647cf3dc901aacec46413d434fd82c7c
kangqiao <kangqiao610@gmail.com>
2018-07-01 22:09:55 +0800

    修改测试页切分脚本.

--- cmds/management/commands/test_pagerect_kexin.py
+++ cmds/management/commands/test_pagerect_kexin.py
@@ -1,6 +1,7 @@
 from django.core.management.base import BaseCommand, CommandError
 from django.conf import settings
 from tdata.models import *
+from bulk_update.helper import bulk_update
 from segment.models import PageRect, PageRectStatus
 from cmds.management.commands.pageReactArith import loadImg
 from cmds.management.commands.downImg import download_img
@@ -8,17 +9,18 @@ from cmds.management.commands.downImg import download_img
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        toCutPageList = PageRect.objects.filter(status=PageRectStatus.CUT_UNCOMPLETED)
+        #toCutPageList = PageRect.objects.filter(status=PageRectStatus.CUT_UNCOMPLETED)
+        page = PageRect.objects.get(code='QS000270_V18_R1_P47')
         updates = []
-        for page in toCutPageList:
-            img = download_img(page.img_path)  # 下载图片存储到本地，并返回路径
-            zuobiaoStr = loadImg(img[0])  # 调用切框的算法，返回坐标值
-            print(zuobiaoStr)  # 139,39,140,25
-            print("=============11111============")
-            array = zuobiaoStr.split(',')
-            page.x = array[0]
-            page.y = array[1]
-            page.w = array[2]
-            page.h = array[3]
-            updates.append(page)
-        PageRect.objects.bulk_update(updates)
+        #for page in toCutPageList:
+        img = download_img(page.img_path)  # 下载图片存储到本地，并返回路径
+        zuobiaoStr = loadImg(img[0])  # 调用切框的算法，返回坐标值
+        print(zuobiaoStr)  # 139,39,140,25
+        print("=============11111============")
+        array = zuobiaoStr.split(',')
+        page.x = array[0]
+        page.y = array[1]
+        page.w = array[2]
+        page.h = array[3]
+        updates.append(page)
+        bulk_update(updates)
